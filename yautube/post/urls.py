7601d656f33f3eaa5e1e6043bab685ccec997b16
teex124 <danil.aslamov@list.ru>
2025-05-15 08:46:57 +0300

    v. 0.2

--- yautube/post/urls.py
+++ yautube/post/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path 
 from . import views
 
+app_name = 'post' 
+
+
 urlpatterns = [
-    path('groups/<slug:pk>/', views.group_posts),
-    path('', views.index)
+    path('groups/<slug:pk>/', views.group_posts, name='group_page'),
+    path('', views.index, name='main_page')
 ]
