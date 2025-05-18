035d9f23e5e06ae6edc8827d479380bbd3707d9f
teex124 <danil.aslmov@list.ru>
2025-05-18 21:04:22 +0300

    fix commit

--- yautube/post/admin.py
+++ yautube/post/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'author','text','pub_date')
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Post, PostAdmin)
+
